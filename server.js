da1e04a3cda17057718c4e6cce4431d900766ec6
mufaisa <mfkamalimdia@gmail.com>
2024-07-10 19:47:13 +0530

    changes

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(express.static('Public'))
 app.use(express.json())
 
 app.use(cors({
-    origin: [`http://localhost:3000`, `http://localhost:3000/home`, `http://localhost:3000/myprofile/:bid`],
+    origin: [`http://localhost:3000`, `http://localhost:3000/home`, `http://localhost:3000/myprofile/:bid`, `https://batman-frontend-pi.vercel.app`],
 
     methods: [`GET`, `POST`, `PUT`, `DELETE`],
 
